2c0aeb6f31c1aba9582b0dc8f193e891da0bf1e9
Xuejin Gao <xuejingao@gmail.com>
2021-04-24 05:07:18 -0400

    Modified the signin button to redirect

--- coffee_chat/src/pages/Landing_Page/LandingInfo.js
+++ coffee_chat/src/pages/Landing_Page/LandingInfo.js
@@ -1,35 +1,51 @@
-import React from 'react';
-
-// import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 // import ReactDOM from 'react-dom';
-// import './LandingTop.css';
+import './LandingTop.css';
 import new_york from '../../images/coffee.jpeg'
 
-// import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
-// import Bounce from 'react-reveal/Bounce';
 
-export default function LandingInfo()
-{
-    return(
-        <>
-            <div>
-                <p className ="landing_coffee_chat" id="coffee_chat_landing">CoffeeChat</p>
-                <div id="buttons">
-                    {/* <Link to="/error404" className="btn btn-primary">test</Link> */}
-                    <button id ="login_button">Login</button>
-                    <button id ="sign_up">Sign Up</button>
-                </div>
-                <Fade right cascade>
-                <div id="quote_subquote">
-                    <h1 id="landing_quote">Connecting students with professionals</h1>
-                    <h3 id="dare">bringing together the minds of today with the minds of tomorrow</h3>
-                </div>
-                </Fade>
-                <Fade>
-                    <img id="new_york" src={new_york} alt="New York City" /> 
-                </Fade>
-            </div>
-        </>
-    )
+class LandingInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.state = {
+      redirect: false,
+    }
+  }
+
+  handleLogin = () => {
+    console.log('redirect');
+    this.setState({ redirect: true });
+  }
+
+  render () {
+    if (this.state.redirect === true) {
+      return <Redirect to="/sign-in" />
+    }
+    return (
+      <>
+        <div>
+          <p className ="landing_coffee_chat" id="coffee_chat_landing">CoffeeChat</p>
+          <div id="buttons">
+            <button id="login_button" onClick={this.handleLogin}>Login/Sign-up</button>
+            {/* <button id ="sign_up">Sign Up</button> */}
+          </div>
+          <Fade right cascade>
+          <div id="quote_subquote">
+            <h1 id="landing_quote">Connecting students with professionals</h1>
+            <h3 id="dare">bringing together the minds of today with the minds of tomorrow</h3>
+          </div>
+          </Fade>
+          <Fade>
+            <img id="new_york" src={new_york} alt="New York City" /> 
+          </Fade>
+        </div>
+      </>
+    );
+  }
 }
+
+
+export default LandingInfo;
