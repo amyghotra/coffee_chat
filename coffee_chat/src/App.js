944caa2baa1285d61660aed098e4a6760b51d415
Xuejin Gao <xuejingao@gmail.com>
2021-04-23 21:51:55 -0400

    Renamed router

--- coffee_chat/src/App.js
+++ coffee_chat/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 import AppRouter from './router';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <AppRouter />
-    </BrowserRouter>
+    </Router>
   );
 }
 
