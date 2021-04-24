9fbfcd850b8ccf6543828822ac6b6db8278d642b
Amy Ghotra <amy.ghotra8@gmail.com>
2021-04-24 14:52:33 -0400

    student and professional profile pages altered

--- coffee_chat/src/router.jsx
+++ coffee_chat/src/router.jsx
@@ -9,6 +9,7 @@ import ProfessionalProfile from './pages/Professional_Profile/index'
 import ProEditInfo from './pages/Professional_Profile/ProEditInfo'
 import ProEditSched from './pages/Professional_Profile/ProEditSched'
 import PublicProInfo from './pages/PublicViews/ProfessionalPublicView/index.jsx'
+import StudentEditInfo from  './pages/Student_Profile_Page/StudentEditProfile'
 
 const AppRouter = () => (
   <Switch>
@@ -17,6 +18,7 @@ const AppRouter = () => (
     <Route exact path="/error404" component={Error404Page} />
     <Route exact path="/studentprofile" component={Student_Profile} />
     <Route exact path="/professionalprofile" component={ProfessionalProfile} />
+    <Route exact path="/student_edit" component={StudentEditInfo} />
     <Route exact path="/pro-edit" component={ProEditInfo} />
     <Route exact path="/schedit" component={ProEditSched} />
     <Route exact path="/propub" component={PublicProInfo} />
