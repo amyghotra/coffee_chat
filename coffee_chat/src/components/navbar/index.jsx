52687b6256abb134c6ec575d2b9ede9a3f1e5131
Xuejin Gao <xuejingao@gmail.com>
2021-04-24 21:28:07 -0400

    Added dropdown menu for nav

--- coffee_chat/src/components/navbar/index.jsx
+++ coffee_chat/src/components/navbar/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { makeStyles } from "@material-ui/core";
 import {
   Collapse,
   Navbar,
@@ -6,30 +7,62 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from 'reactstrap';
 import './nav.css'
 import logo from  '../../images/logo.png'
 
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor:"#283044",
+    color:"white",
+  },
+  navlinks: {
+    margin: "0 2em",
+  },
+  dropdown: {
+    backgroundColor: "#283044",
+    color: "green",
+  },
+  whiteText: {
+    color: "#ffffff",
+  }
+});
 
 const NavBar = () => {
+  const classes = useStyles();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div id="universal_navbar">
-      <Navbar style={{backgroundColor:"#283044", color:"white"}} light expand="md">
+      <Navbar className={classes.navbar} light expand="md">
       <img id="nav_img" src={logo} alt="Corgi in a Coffee cup sticker" />
         <NavbarBrand style={{color:"white"}} className="nav_text" href="/landing">CoffeeChat</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
-          <NavItem>
-              <NavLink style={{color:"white"}} className="nav_text" href="/landing">About Us</NavLink>
-          </NavItem>
-          <NavItem>
-              <NavLink style={{color:"white"}} className="nav_text" href="/">Logout</NavLink>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle style={{color:"white"}} nav caret className={classes.whiteText}>
+                Extras
+              </DropdownToggle>
+              <DropdownMenu className={classes.dropdown}>
+                <DropdownItem>
+                  <NavLink style={{color:"white"}} className="nav_text" href="/topcompanies">Company Rankings</NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                  <NavLink style={{color:"white"}} className="nav_text" href="/landing">About Us</NavLink>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+                <NavLink style={{color:"white"}} className="nav_text" href="/">Logout</NavLink>
           </NavItem>
           </Nav>
         </Collapse>
