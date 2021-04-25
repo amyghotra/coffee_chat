53c845918912cc7af53c7978434f3c3c33af97ef
Xuejin Gao <xuejingao@gmail.com>
2021-04-24 20:13:02 -0400

    Modified colors

--- coffee_chat/src/pages/TopCompany_Page/index.jsx
+++ coffee_chat/src/pages/TopCompany_Page/index.jsx
@@ -12,6 +12,7 @@ import {
   Typography,
 } from "@material-ui/core";
 
+import { COLOR_PALETTE } from "../../utils/theme";
 import Navbar from "../../components/navbar";
 
 const useStyles = makeStyles({
@@ -20,17 +21,39 @@ const useStyles = makeStyles({
   },
   center: {
     textAlign: "center",
-  }
+  },
+  title: {
+    margin: "1em auto .25em",
+  },
+  tableHeader: {
+    backgroundColor: COLOR_PALETTE.Primary,
+  },
 });
 
 const DEMO_COMPANIES = [
   {
     name: "Company1",
-    hours: 15,
+    hours: 55,
   },
   {
     name: "Company2",
-    hours: 10,
+    hours: 50,
+  },
+  {
+    name: "Company3",
+    hours: 45,
+  },
+  {
+    name: "Company4",
+    hours: 40,
+  },
+  {
+    name: "Company5",
+    hours: 35,
+  },
+  {
+    name: "Company6",
+    hours: 30,
   }
 ];
 
@@ -54,33 +77,37 @@ const CompanyList = () => {
   }, []);
 
   return (
-    <TableContainer component={Paper} >
-      <Table className={classes.table} >
-        <TableHead>
-          <TableRow>
-            <TableCell>Rank</TableCell>
-            <TableCell>Company</TableCell>
-            <TableCell>Hours</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {companies.length === 0? 
-            (<NoResult />) : 
-            companies.map((company, index) => {
-              const { name, hours } = company;
-              return (
-                <TableRow key={index}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell component="th" scope="row">{name}</TableCell>
-                  <TableCell>{hours}</TableCell>
-                </TableRow>
-                
-              ) 
-            })
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <Typography variant="h3" className={classes.title} >
+        Rankings of Helpful Companies
+      </Typography>
+      <TableContainer component={Paper} >
+        <Table className={classes.table} >
+          <TableHead>
+            <TableRow classname={classes.tableHeader}>
+              <TableCell>Rank</TableCell>
+              <TableCell>Company</TableCell>
+              <TableCell>Hours</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {companies.length === 0? 
+              (<NoResult />) : 
+              companies.map((company, index) => {
+                const { name, hours } = company;
+                return (
+                  <TableRow key={index}>
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell component="th" scope="row">{name}</TableCell>
+                    <TableCell>{hours}</TableCell>
+                  </TableRow>
+                ) 
+              })
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
@@ -89,9 +116,6 @@ const TopCompanyPage = () => {
     <div>
       <Navbar />
       <Container fixed >
-        <Typography variant="h3" gutterBottom>
-          Rankings of Helpful Companies
-        </Typography>
         <CompanyList />
       </Container>
     </div>
