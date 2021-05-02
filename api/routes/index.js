df5773d989d92a4a6efaba8eec337decfb4fca60
Xuejin Gao <xuejingao@gmail.com>
2021-05-02 13:03:36 -0400

    added company router

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,9 +4,11 @@ const router = express();
 // Subrouters
 const registerRouter = require("./jwtauth"); // login register route
 const studentRouter = require("./students");
+const companyRouter = require("./companies");
 
 // Mount Routers
 router.use("/auth", registerRouter);
 router.use("/students", studentRouter);
+router.use("/companies", companyRouter);
 
 module.exports = router;
