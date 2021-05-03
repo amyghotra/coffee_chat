fe60d8919b4eaae126b3673c1c26e68d4244e7ea
Xuejin Gao <xuejingao@gmail.com>
2021-05-03 19:29:18 -0400

    Connected remote

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 
 // Routes
+const echoRouter = require("./echo");
 const jwtauthRouter = require("./jwtauth");
 const dashboardRouter = require("./dashboard");
+const { route } = require("./jwtauth");
 
 // Connect routes
 router.use("/auth", jwtauthRouter);
 router.use("/dashboard", dashboardRouter);
+
+// Testing purposes
+router.use("/echo", echoRouter);
 
 module.exports = router;
