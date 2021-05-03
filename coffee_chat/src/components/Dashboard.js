7f50bdb9032fe0d3c326fc9e18a562e90acb80e2
Amy Ghotra <amy.ghotra8@gmail.com>
2021-05-03 17:50:21 -0400

    register complete; redirect to a dashboard

--- coffee_chat/src/components/Dashboard.js
+++ coffee_chat/src/components/Dashboard.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {Redirect, location,useHistory} from 'react-router-dom'
 
-export default function Dashboard(){
+export default function Dashboard(props){
+    const history = useHistory()
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    useEffect(() => {
+        if(props.location.state.isAuth !== 'undefined') {
+            setIsAuthenticated(props.location.state.isAuth)
+        } else {
+            // setIsAuthenticated(false)
+            history.push('/landing')
+        }
+    }, [])
+
+    if(!isAuthenticated) {
+        <Redirect to="/landing" />
+    }
+
     return(
         <>
-            <h1>dashboard</h1>
+            <h1>Dashboard</h1>
         </>
     )
 }
