4636fcf149c048f555b284a29c82f4f9baeae720
Xuejin Gao <xuejingao@gmail.com>
2021-05-04 00:30:16 -0400

    Fixed database, dml and initialized db

--- api/database/database.sql
+++ api/database/database.sql
@@ -1,9 +1,18 @@
-CREATE DATABASE coffee_chat;
+CREATE DATABASE coffeechat;
+
+DROP TABLE if exists users CASCADE;
+DROP TABLE if exists students CASCADE;
+DROP TABLE if exists professionals CASCADE;
+DROP TABLE if exists meetings CASCADE;
+DROP TABLE if exists proAvailability CASCADE;
+DROP TABLE if exists companies CASCADE;
+DROP TABLE if exists worksAt CASCADE;
+
 
 CREATE extension if not exists "uuid-ossp";
 
 CREATE TABLE users(
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
     name VARCHAR(64) NOT NULL,
     email VARCHAR(64) NOT NULL,
     social VARCHAR(64),
@@ -28,15 +37,15 @@ CREATE TABLE meetings(
     pro_id uuid,
     date DATE,
     time TIME,
-    FOREIGN KEY (student_id) references students(id),
-    FOREIGN KEY (pro_id) references professionals(id)
+    FOREIGN KEY (student_id) references users(id),
+    FOREIGN KEY (pro_id) references users(id)
 );
 
 CREATE TABLE proAvailability(
     pro_id uuid,
     date DATE,
     time TIME,
-    FOREIGN KEY (pro_id) references professionals(id)
+    FOREIGN KEY (pro_id) references users(id)
 );
 
 CREATE TABLE companies(
@@ -49,7 +58,7 @@ CREATE TABLE worksAt(
     pro_id uuid,
     company_id uuid,
     position VARCHAR(64),
-    FOREIGN KEY (pro_id) references professionals(id),
+    FOREIGN KEY (pro_id) references users(id),
     FOREIGN KEY (company_id) references companies(id)
 );
 
