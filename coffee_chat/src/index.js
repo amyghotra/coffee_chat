765914132da0a42b3951c3d2c8260a72495953be
Xuejin Gao <xuejingao@gmail.com>
2021-05-11 16:00:39 -0400

    Added user context

--- coffee_chat/src/index.js
+++ coffee_chat/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import UserContextProvider from './contexts/users';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-   <Router>
-    <App />
-   </Router>
+    <Router>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
