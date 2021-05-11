02dbb9a32ccd72fa0529bfae973a2f2304822b1d
Xuejin Gao <xuejingao@gmail.com>
2021-05-11 15:41:03 -0400

    Added user context

---
+++ coffee_chat/src/contexts/users.js
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { GetVertification } from '../services';
+
+const uType = {
+  GUEST: 'guest',
+  STUDENT: 'student',
+  PRO: 'professional',
+};
+
+const unimplemented = () => {
+  throw new Error('Context has not been initialized');
+};
+
+const UserContext = createContext({
+  userType: null,
+  authorized: false,
+  selectedProfessional: null,
+  handleSetProfessional: unimplemented,
+  handleAuthenicate: unimplemented,
+  handleLogout: unimplemented,
+});
+
+export const UserContextProvider = ({ children }) => {
+  const [userType, setUserType] = useState(uType.GUEST);
+  const [authorized, setAuthorized] = useState(false);
+  const [selectedProfessional, setSelectedProfessional] = useState();
+
+  useEffect(() => {
+    const handleRetrieveUserType = async (token) => {
+      const userT = await GetVertification(token);
+      // console.log(userT.userType);
+      if (userT.userType === 'student') {
+        setUserType(uType.STUDENT);
+        setAuthorized(true);
+      } else if (userT.userType === 'professional') {
+        setUserType(uType.PRO);
+        setAuthorized(true);
+      }
+    };
+
+    handleRetrieveUserType(localStorage.token);
+  }, []);
+
+  const handleSetProfessional = (professionalId) => {
+    setSelectedProfessional(professionalId);
+  };
+
+  const handleAuthenicate = () => {
+    setAuthorized(true);
+  };
+
+  const handleLogout = () => {
+    setAuthorized(false);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        userType,
+        authorized,
+        selectedProfessional,
+        handleSetProfessional,
+        handleAuthenicate,
+        handleLogout,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
