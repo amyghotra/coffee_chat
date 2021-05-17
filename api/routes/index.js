dc1be106014cf8fd5300258c6c7d694f5354c83a
Amy Ghotra <amy.ghotra8@gmail.com>
2021-05-16 23:28:44 -0400

    display student's meetings and let them cancel

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,6 +11,7 @@ const updateStudentInfo = require('./updateStudentInfo');
 const updateProInfo = require('./updateProInfo');
 const proSchedule = require('./proSchedule');
 const scheduleMeetings = require('./scheduleMeetings')
+const studentMeetings = require('./studentMeetings')
 
 // Connect routes
 router.use('/auth', jwtauthRouter);
@@ -22,6 +23,7 @@ router.use('/updateStudentInfo', updateStudentInfo);
 router.use('/updateProInfo', updateProInfo);
 router.use('/proSchedule', proSchedule);
 router.use('/scheduleMeetings', scheduleMeetings);
+router.use('/studentMeetings', studentMeetings);
 
 // Testing purposes
 router.use('/echo', echoRouter);
