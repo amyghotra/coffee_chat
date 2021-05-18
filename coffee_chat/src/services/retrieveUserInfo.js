f8b90011e17e9660a5fa778681b453b665f2eea2
Xuejin Gao <xuejingao@gmail.com>
2021-05-18 18:04:58 -0400

    fixed professional page on reclick

--- coffee_chat/src/services/retrieveUserInfo.js
+++ coffee_chat/src/services/retrieveUserInfo.js
@@ -7,3 +7,11 @@ export const GetStudentUserInfo = async (token) => {
 
   return info.data.results;
 };
+
+export const GetProfessionalUserInfo = async (token) => {
+  const info = await axios.get('http://localhost:5000/auth/info/pro', {
+    headers: { jwtToken: token },
+  });
+
+  return info.data.results;
+};
